31ef9d5c8f320acdedc6112fb20b94a837fe4abc
Alexandr Lachin <alexandrlachin989@gmail.com>
2025-08-01 09:05:11 +0300

    Update

--- Public/index.php
+++ Public/index.php
@@ -9,22 +9,7 @@ if($SECURYTI){
     }
 }else{
 
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-    <link rel="shortcut icon" type="image/png" href="./images/favicon.ico">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <title>Altamed WhatsApp</title>
-<script defer="defer" src="./JS/index8b759436954c71c1359a.js"></script><link href="./CSS/index8b759436954c71c1359a.css" rel="stylesheet"></head>
-    <body>
-    </body>
-</html>
-
-<?php } 
+?> <!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width,initial-scale=1"><link rel="shortcut icon" type="image/png" href="./images/favicon.ico"><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous"><title>Altamed WhatsApp</title><script defer="defer" src="./JS/index32311c53caadb3052b31.js"></script><link href="./CSS/index32311c53caadb3052b31.css" rel="stylesheet"></head><body></body></html> <?php } 
 if(error_get_last()){
   $Arr =  error_get_last(); // получаем массив ошбки
   $f =  fopen("./php/log_error.txt", "a+"); //открываем файл для записи курсов в конце на новой строке
